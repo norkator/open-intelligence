7178a0f29452c30cfb36a99d1d0ac3ff90deed6b
Norkator <norkator@gmail.com>
2020-01-06 21:51:50 +0200

    Playing with classifiers at person detection side

--- Person.py
+++ Person.py
@@ -1,10 +1,13 @@
 import os
+import time
+
 from module import fileutils, person_detection, configparser
 from objects import File
 
 # Configure input image folder
 image_folders = [
-    os.getcwd() + '/output/person/',
+    # os.getcwd() + '/output/person/',
+    os.getcwd() + '/images/',
 ]
 
 
@@ -18,26 +21,31 @@ def app():
         fileutils.create_directory(image_folder + 'processed/')
         # Process files
         for file_name in fileutils.get_camera_image_names(image_folder):
-            gm_time = fileutils.get_file_create_time(image_folder, file_name)
-            file = File.File(
-                image_folder,
-                file_name,
-                fileutils.get_file_extension(image_folder, file_name),
-                fileutils.get_file_create_year(gm_time),
-                fileutils.get_file_create_month(gm_time),
-                fileutils.get_file_create_day(gm_time),
-                fileutils.get_file_create_hour(gm_time),
-                fileutils.get_file_create_minute(gm_time),
-                fileutils.get_file_create_second(gm_time)
-            )
-            image_file_objects.append(file)
+            if file_name != 'processed':
+                gm_time = fileutils.get_file_create_time(image_folder, file_name)
+                file = File.File(
+                    image_folder,
+                    file_name,
+                    fileutils.get_file_extension(image_folder, file_name),
+                    fileutils.get_file_create_year(gm_time),
+                    fileutils.get_file_create_month(gm_time),
+                    fileutils.get_file_create_day(gm_time),
+                    fileutils.get_file_create_hour(gm_time),
+                    fileutils.get_file_create_minute(gm_time),
+                    fileutils.get_file_create_second(gm_time)
+                )
+                image_file_objects.append(file)
 
     # Analyze image objects
     for image_object in image_file_objects:
         try:
             person_detection.analyze_image(
                 image_object,
-                configparser.app_config()['movetoprocessed'] == 'True'
+                configparser.app_config()['move_to_processed'] == 'True'
             )
-        except:
-            print('')
+        except Exception as e:
+            print(e)
+        time.sleep(1)
+
+
+app()
--- module/person_detection.py
+++ module/person_detection.py
@@ -3,20 +3,21 @@ import cv2
 
 # Paths
 person_path = os.getcwd() + '/output/person/'
+classifier_path = os.getcwd() + '/classifiers/'
 
 
 def analyze_image(image_object, bool_move_processed):
-    object_cascade = cv2.CascadeClassifier('person_cascade.xml')
+    object_cascade = cv2.CascadeClassifier(classifier_path + 'haarcascade_frontalface_alt.xml')
 
-    ret, img = cv2.imread(image_object.file_path + image_object.file_name)
+    img = cv2.imread(image_object.file_path + image_object.file_name)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # image, reject levels level weights.
-    img_object = object_cascade.detectMultiScale(gray, 50, 50)
+    faces = object_cascade.detectMultiScale(gray, 1.3, 5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
     # add this
-    for (x, y, w, h) in img_object:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 1)
 
     cv2.imshow('img', img)
     cv2.waitKey(1)  # no freeze, refreshes for a millisecond
