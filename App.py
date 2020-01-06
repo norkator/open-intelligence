a12ed6d55a64ad0ab0ba8d7fa40fef837932912b
Norkator <norkator@gmail.com>
2020-01-06 10:42:19 +0200

    Program now stays running forever

--- App.py
+++ App.py
@@ -1,34 +1,60 @@
 import os
 from module import fileutils, analyser
 from objects import File
+import sys
+import time
 
 # Configure input image folders
 image_folders = [os.getcwd() + '/images/']
 
-# Image objects stored here
-image_file_objects = []
 
-# Create image objects
-for image_folder in image_folders:
-    # Check for 'processed' folder existence
-    fileutils.create_directory(image_folder + 'processed/')
-    # Process files
-    for file_name in fileutils.get_camera_image_names(image_folder):
-        gm_time = fileutils.get_file_create_time(file_name)
-        file = File.File(
-            image_folder,
-            file_name,
-            fileutils.get_file_extension(file_name),
-            fileutils.get_file_create_year(gm_time),
-            fileutils.get_file_create_month(gm_time),
-            fileutils.get_file_create_day(gm_time),
-            fileutils.get_file_create_hour(gm_time),
-            fileutils.get_file_create_minute(gm_time),
-            fileutils.get_file_create_second(gm_time)
-        )
-        image_file_objects.append(file)
+def app():
+    # Image objects stored here
+    image_file_objects = []
+
+    # Create image objects
+    for image_folder in image_folders:
+        # Check for 'processed' folder existence
+        fileutils.create_directory(image_folder + 'processed/')
+        # Process files
+        for file_name in fileutils.get_camera_image_names(image_folder):
+            gm_time = fileutils.get_file_create_time(file_name)
+            file = File.File(
+                image_folder,
+                file_name,
+                fileutils.get_file_extension(file_name),
+                fileutils.get_file_create_year(gm_time),
+                fileutils.get_file_create_month(gm_time),
+                fileutils.get_file_create_day(gm_time),
+                fileutils.get_file_create_hour(gm_time),
+                fileutils.get_file_create_minute(gm_time),
+                fileutils.get_file_create_second(gm_time)
+            )
+            image_file_objects.append(file)
+
+    # Analyze image objects
+    for image_object in image_file_objects:
+        try:
+            result = analyser.analyze_image(image_object, True)
+        except:
+            print('')
 
 
-# Analyze image objects
-for image_object in image_file_objects:
-    result = analyser.analyze_image(image_object, False)
+# ---------------------------------------------------------------------
+# Keeps program running
+
+def main_loop():
+    while 1:
+        app()
+        print('... running')
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        print >> sys.stderr, '\nExiting by user request.\n'
+        sys.exit(0)
+
+# ---------------------------------------------------------------------
