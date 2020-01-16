ee94b5f9199b7e588fd7ec190a71729f233c1d38
Norkator <norkator@gmail.com>
2020-01-16 18:05:59 +0200

    Added .rollback() for db connection. Had error case when inserts no longer got in because did not rollback exception.

--- module/database.py
+++ module/database.py
@@ -18,23 +18,27 @@ def db_connected():
 
 
 def insert_value(name, label, file_path, file_name, year, month, day, hour, minute, second, detection_result):
-    cursor = connection.cursor()
+    try:
+        cursor = connection.cursor()
 
-    # Datetime format: '2011-05-16 15:36:38'
-    file_create_date = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second
+        # Datetime format: '2011-05-16 15:36:38'
+        file_create_date = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second
 
-    # print(file_create_date)
+        # print(file_create_date)
 
-    # Query
-    postgres_insert_query = """ INSERT INTO data (name, label, file_path, file_name, file_create_date, detection_completed, detection_result) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+        # Query
+        postgres_insert_query = """ INSERT INTO data (name, label, file_path, file_name, file_create_date, detection_completed, detection_result) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
 
-    # Variables
-    record_to_insert = (name, label, file_path, file_name, file_create_date, 1, detection_result)
+        # Variables
+        record_to_insert = (name, label, file_path, file_name, file_create_date, 1, detection_result)
 
-    # Execute insert
-    cursor.execute(postgres_insert_query, record_to_insert)
+        # Execute insert
+        cursor.execute(postgres_insert_query, record_to_insert)
 
-    connection.commit()
-    count = cursor.rowcount
+        connection.commit()
+        count = cursor.rowcount
 
-    # print(count, "Database record inserted")
+        # print(count, "Database record inserted")
+    except psycopg2.DatabaseError as error:
+        connection.rollback()
+        print(error)
