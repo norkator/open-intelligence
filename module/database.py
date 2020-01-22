b994f6b492a480328671fe8b86a97e459f3ed8d2
Norkator <norkator@gmail.com>
2020-01-22 20:54:19 +0200

    Fix attempt for postgres connection issues

--- module/database.py
+++ module/database.py
@@ -2,23 +2,28 @@ import psycopg2
 from module import configparser
 
 params = configparser.db_config()
-connection = psycopg2.connect(**params)
 
 
+# connection = psycopg2.connect(**params)
 # print(connection.get_backend_pid())
 
 
 def db_connected():
+    connection = psycopg2.connect(**params)
     try:
         cur = connection.cursor()
         cur.execute('SELECT 1')
+        cur.close()
         return True
     except psycopg2.OperationalError:
         return False
+    finally:
+        connection.close()
 
 
 def insert_value(name, label, file_path, file_name, year, month, day, hour, minute, second, file_name_cropped,
                  detection_result):
+    connection = psycopg2.connect(**params)
     try:
         cursor = connection.cursor()
 
@@ -39,13 +44,17 @@ def insert_value(name, label, file_path, file_name, year, month, day, hour, minu
         connection.commit()
         count = cursor.rowcount
 
+        cursor.close()
         # print(count, "Database record inserted")
     except psycopg2.DatabaseError as error:
         connection.rollback()
         print(error)
+    finally:
+        connection.close()
 
 
 def get_super_resolution_images_to_compute():
+    connection = psycopg2.connect(**params)
     try:
         cursor = connection.cursor()
         sr_work_query = "SELECT id, label, file_name_cropped, detection_result FROM data WHERE sr_image_computed = 0 ORDER BY id ASC LIMIT 10"
@@ -58,21 +67,27 @@ def get_super_resolution_images_to_compute():
         #    print("Label = ", row[1])
         #    print("Cropped  = ", row[2])
         #    print("Detection result  = ", row[3], "\n")
-
+        cursor.close()
         return sr_work_records
     except psycopg2.DatabaseError as error:
         connection.rollback()
         print(error)
+    finally:
+        connection.close()
 
 
 def update_super_resolution_row_result(detection_result, sr_image_name, id):
+    connection = psycopg2.connect(**params)
     try:
         cursor = connection.cursor()
 
         sr_update_query = """UPDATE data SET sr_image_computed = 1, detection_after_sr_completed = 1, detection_result = %s, sr_image_name = %s WHERE id = %s"""
         cursor.execute(sr_update_query, (detection_result, sr_image_name, id))
         connection.commit()
+        cursor.close()
         # count = cursor.rowcount
     except psycopg2.DatabaseError as error:
         connection.rollback()
         print(error)
+    finally:
+        connection.close()
