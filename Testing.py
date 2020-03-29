9c05d89e4dfab46d4fa1c1e1d6850a3561acca19
Norkator <norkator@gmail.com>
2020-03-29 17:31:43 +0300

    Implementation of rotation system to rotate vehicle images so that OCR may detect something. Worked pretty nice on testing.

---
+++ Testing.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+import imutils
+import numpy as np
+from module import license_plate_detection
+
+file_path = os.getcwd() + '/output/car/' + '2020_03_25_17_42_16_0.jpg'
+
+# load the image from disk
+image = cv2.imread(file_path)
+
+# Rotation with no part of the image is cut off
+for angle in np.arange(-30, 30, 4):
+    rotated = imutils.rotate_bound(image, angle)
+    cv2.imshow("Rotated (Correct)", rotated)
+    cv2.waitKey(0)
