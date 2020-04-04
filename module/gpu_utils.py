236562baf3fc40a241ecb783bcd13fad5396a68e
Norkator <norkator@gmail.com>
2020-04-04 19:58:40 +0300

    Now it started to work but my main GPU is too old. The minimum required Cuda capability is 3.5... need to find better GPU.

--- module/gpu_utils.py
+++ module/gpu_utils.py
@@ -1,1 +1,16 @@
-from tensorflow import keras
+import tensorflow
+# from tensorflow import keras
+from tensorflow.python.client import device_lib
+
+
+def get_available_gpus():
+    # tensorflow.config.experimental.list_physical_devices('GPU')
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+
+def is_gpu_available():
+    return tensorflow.test.is_gpu_available()
+
+
+print(is_gpu_available())
