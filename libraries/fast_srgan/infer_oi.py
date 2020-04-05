a23778d9fa24c04d32d98a388b0be9853776090b
Norkator <norkator@gmail.com>
2020-04-05 12:43:15 +0300

    Testing different config methods

--- libraries/fast_srgan/infer_oi.py
+++ libraries/fast_srgan/infer_oi.py
@@ -1,10 +1,9 @@
 from argparse import ArgumentParser
-import tensorflow
+import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import cv2
 import os
-import keras.backend.tensorflow_backend as ktf
 
 parser = ArgumentParser()
 parser.add_argument('--image_dir', type=str, help='Directory where images are kept.')
@@ -12,6 +11,13 @@ parser.add_argument('--output_dir', type=str, help='Directory where to output hi
 
 # Model path
 model_path_file_name = os.getcwd() + '/libraries/fast_srgan/models/generator.h5'
+
+# Set Keras TensorFlow session config
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.4
+config.gpu_options.allow_growth = True
+tf_session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(tf_session)
 
 
 def process_super_resolution_images(sr_image_objects):
