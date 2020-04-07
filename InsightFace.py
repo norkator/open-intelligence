a97a7636fd635a10e457fdcdefed9ca7e9d6c34e
Norkator <norkator@gmail.com>
2020-04-07 21:11:57 +0300

    More development for testing

--- InsightFace.py
+++ InsightFace.py
@@ -1,24 +1,44 @@
-from module import gpu_utils
+from module import gpu_utils, fileutils
 import insightface
 import cv2
 import os
 
+# Paths
+input_images_path = os.getcwd() + '/output/person/'
+output_images_path = os.getcwd() + '/output/insightface/faces/'
+
+# Check folder existence
+fileutils.create_directory(output_images_path)
+
 # If GPU available, use GPU else CPU
 ctx_id = -1 if gpu_utils.is_gpu_available() is False else 1
 
-# Loading image
-imgSrc = os.getcwd() + '/images/' + '192.168.2.61_01_20200406080028946_MOTION_DETECTION.jpg'
-image = cv2.imread(imgSrc)
-
+# Load model
 model = insightface.model_zoo.get_model('retinaface_r50_v1')
-
 model.prepare(ctx_id=ctx_id, nms=0.4)
 
-boxes, landmarks = model.detect(image, threshold=0.2, scale=1.0)
+# Process images
+for file_name in fileutils.get_camera_image_names(input_images_path):
+    if file_name != 'processed' and file_name != 'Thumbs.db' and file_name.find('.lock') is -1:
+        # Loading image
+        image = cv2.imread(input_images_path + file_name)
+        img_show = image.copy()
 
-# Draw boxes
-for box in boxes:
-    cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
+        # Detect face
+        boxes, landmarks = model.detect(image, threshold=0.2, scale=1.0)
 
-cv2.imshow('image', image)
-cv2.waitKey(0)
+        if len(boxes) > 0:
+            # Save face containing image
+            try:
+                cv2.imwrite(output_images_path + file_name, image)
+            except Exception as e:
+                print(e)
+
+            '''
+            # Draw boxes
+            for box in boxes:
+                cv2.rectangle(img_show, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
+    
+            cv2.imshow('image', img_show)
+            cv2.waitKey(0)
+            '''
