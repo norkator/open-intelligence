fa242c058fd13005e880facdc757d2770bcdb24e
Norkator <norkator@gmail.com>
2020-05-17 10:00:08 +0300

    Smaller image size requirement, more try catch blocks

--- libraries/fast_srgan/infer_oi.py
+++ libraries/fast_srgan/infer_oi.py
@@ -16,73 +16,78 @@ model_path_file_name = os.getcwd() + '/libraries/fast_srgan/models/generator.h5'
 
 # Set Keras TensorFlow session config
 config = tf.compat.v1.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.9  # Use 90%
+config.gpu_options.per_process_gpu_memory_fraction = 0.8  # Use 80%
 config.gpu_options.allow_growth = True
 tf_session = tf.compat.v1.Session(config=config)
 tf.compat.v1.keras.backend.set_session(tf_session)
 
 
 def process_super_resolution_images(sr_image_objects):
-    # Load model to memory
-    # Change model input shape to accept all size inputs
-    model = keras.models.load_model(model_path_file_name, compile=False)
-    inputs = keras.Input((None, None, 3))
-    output = model(inputs)
-    model = keras.models.Model(inputs, output)
+    try:
 
-    # Loop over all images
-    # Input and output image is full path + filename including extension
-    for sr_image_object in sr_image_objects:
-        # print(sr_image_object.output_image)
-        print('Processing file: ' + os.path.basename(sr_image_object.input_image))
+        # Load model to memory
+        # Change model input shape to accept all size inputs
+        model = keras.models.load_model(model_path_file_name, compile=False)
+        inputs = keras.Input((None, None, 3))
+        output = model(inputs)
+        model = keras.models.Model(inputs, output)
 
-        # We may not have image available at all, pass
-        try:
-            # Read image
-            low_res = cv2.imread(sr_image_object.input_image, 1)
+        # Loop over all images
+        # Input and output image is full path + filename including extension
+        for sr_image_object in sr_image_objects:
+            # print(sr_image_object.output_image)
+            print('Processing file: ' + os.path.basename(sr_image_object.input_image))
 
-            # Get image size details
-            original_h, original_w, original_c = low_res.shape
-            print('Sr processing img size: ' + str(original_h) + ':' + str(original_w))
+            # We may not have image available at all, pass
+            try:
+                # Read image
+                low_res = cv2.imread(sr_image_object.input_image, 1)
 
-            # Check if image is not too big
-            # Original size was 1200 but testing with smaller
-            if original_w < 1000 and original_h < 1000:
-                # Convert to RGB (opencv uses BGR as default)
-                low_res = cv2.cvtColor(low_res, cv2.COLOR_BGR2RGB)
+                # Get image size details
+                original_h, original_w, original_c = low_res.shape
+                print('Sr processing img size: ' + str(original_h) + ':' + str(original_w))
 
-                # Rescale to 0-1.
-                low_res = low_res / 255.0
+                # Check if image is not too big
+                # Original size was 1200 but testing with smaller
+                if original_w < 700 and original_h < 700:
+                    # Convert to RGB (opencv uses BGR as default)
+                    low_res = cv2.cvtColor(low_res, cv2.COLOR_BGR2RGB)
 
-                # Get super resolution image
-                sr = model.predict(np.expand_dims(low_res, axis=0))[0]
+                    # Rescale to 0-1.
+                    low_res = low_res / 255.0
 
-                # Rescale values in range 0-255
-                sr = ((sr + 1) / 2.) * 255
+                    # Get super resolution image
+                    sr = model.predict(np.expand_dims(low_res, axis=0))[0]
 
-                # Convert back to BGR for opencv
-                sr = cv2.cvtColor(sr, cv2.COLOR_RGB2BGR)
+                    # Rescale values in range 0-255
+                    sr = ((sr + 1) / 2.) * 255
 
-                # Save the results:
-                cv2.imwrite(sr_image_object.output_image, sr)
+                    # Convert back to BGR for opencv
+                    sr = cv2.cvtColor(sr, cv2.COLOR_RGB2BGR)
 
-                # Save sr image data to object
-                sr_image_object.set_sr_image_data(sr)
+                    # Save the results:
+                    cv2.imwrite(sr_image_object.output_image, sr)
 
-                # Clear sr object
-                sr = None
-            else:
-                # Save original image
-                sr_image_object.set_sr_image_data(low_res)
+                    # Save sr image data to object
+                    sr_image_object.set_sr_image_data(sr)
 
-        except Exception as e:
-            print(e)
+                    # Clear sr object
+                    sr = None
+                else:
+                    # Save original image
+                    sr_image_object.set_sr_image_data(low_res)
 
-    # Clear model
-    tf.compat.v1.reset_default_graph()  # Try free memory from Tensorflow
-    kb.clear_session()  # Clear Keras session
-    # model = None
-    del model
-    gc.collect()
+            except Exception as e:
+                print(e)
+
+        # Clear model
+        tf.compat.v1.reset_default_graph()  # Try free memory from Tensorflow
+        kb.clear_session()  # Clear Keras session
+        # model = None
+        del model
+        gc.collect()
+
+    except Exception as e:
+        print(e)
 
     return sr_image_objects
