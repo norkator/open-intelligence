1a6056dbcb0025fcb08630e20a408353aede3f8b
Norkator <55850510+norkator@users.noreply.github.com>
2020-05-21 21:06:18 +0300

    Update ALPRTraining.py

--- ALPRTraining.py
+++ ALPRTraining.py
@@ -4,8 +4,14 @@ from libraries.nitramite_alpr import export
 export.app()
 '''
 
-# '''
+'''
 from libraries.nitramite_alpr import train
 
 train.train()
-# '''
+'''
+
+'''
+from libraries.nitramite_alpr import test
+
+test.test(100)
+'''
