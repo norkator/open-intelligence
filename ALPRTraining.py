0c9e99e0d105e3d7c6c70c4db73eed9b677b1a8f
Norkator <55850510+norkator@users.noreply.github.com>
2020-05-21 13:16:08 +0300

    Update ALPRTraining.py

--- ALPRTraining.py
+++ ALPRTraining.py
@@ -18,6 +18,19 @@ Path(output_lp_training_test_path).mkdir(parents=True, exist_ok=True)
 Path(output_lp_training_lp_path).mkdir(parents=True, exist_ok=True)
 
 
+# Original images are resize'd,
+# bounding box must be adjusted
+def calculate_resize_bbox(orig_w, orig_h, new_w, new_h, orig_x, orig_y, orig_x2, orig_y2):
+    new_x = int((new_w / orig_w) * orig_x)
+    new_y = int((new_h / orig_h) * orig_y)
+    new_x2 = int((new_w / orig_w) * orig_x2)
+    new_y2 = int((new_h / orig_h) * orig_y2)
+
+    r = [new_x, new_y, new_x2, new_y2]
+    print(r)
+    return r
+
+
 def app():
     rows = database.get_labeled_for_training_lp_images()
     lp_training_image_objects = []
@@ -48,11 +61,31 @@ def app():
         for tio in lp_training_image_objects:
             # Write lp crop image for viewing purposes
             img = cv2.imread(tio.file_full_path)
+
+            '''
             lp_crop = img[tio.labeling_image_y:tio.labeling_image_y2, tio.labeling_image_x:tio.labeling_image_x2]
             try:
                 cv2.imwrite(output_lp_training_lp_path + '/' + tio.file_name_cropped, lp_crop)
             except Exception as e:
                 print(e)
+            '''
+
+            original_h, original_w, original_c = img.shape
+            new_size = 256  # will be square, same width, same height
+            resize = cv2.resize(img, (new_size, new_size))
+            new_bbox = calculate_resize_bbox(
+                original_w, original_h, new_size, new_size,
+                tio.labeling_image_x, tio.labeling_image_y, tio.labeling_image_x2, tio.labeling_image_y2
+            )
+
+            lp_crop = resize[new_bbox[1]:new_bbox[3], new_bbox[0]:new_bbox[2]]
+
+            '''
+            cv2.imshow("OrigImg", img)
+            cv2.imshow("256Img", resize)
+            cv2.imshow("LpCrop", lp_crop)
+            cv2.waitKey(0)
+            '''
 
         print('[info] export completed')
     else:
