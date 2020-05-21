d489b29ad9c4c81594f8684fa62f5ede5ded1f33
Norkator <55850510+norkator@users.noreply.github.com>
2020-05-21 11:32:02 +0300

    Update ALPRTraining.py

--- ALPRTraining.py
+++ ALPRTraining.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from module import database
 from objects import TrainingFile
+import cv2
 
 # Paths
 output_root_folder_path = os.getcwd() + '/output/'
@@ -30,7 +31,7 @@ def app():
 
         # Make objects
         lp_training_image_object = TrainingFile.TrainingFile(
-            id, label, file_name_cropped,
+            id, file_name_cropped, label,
             labeling_image_x, labeling_image_y, labeling_image_x2, labeling_image_y2,
             input_image
         )
@@ -38,10 +39,16 @@ def app():
 
     # Export process
     if len(lp_training_image_objects) > 0:
-        for trainingFile in lp_training_image_objects:
-            print(trainingFile.file_full_path)
+        for tio in lp_training_image_objects:
+            img = cv2.imread(tio.file_full_path)
+            lp_crop = img[tio.labeling_image_y:tio.labeling_image_y2, tio.labeling_image_x:tio.labeling_image_x2]
+            try:
+                cv2.imwrite(output_lp_training_path + '/' + tio.file_name_cropped, lp_crop)
+            except Exception as e:
+                print(e)
+        print('[info] export completed')
     else:
-        print('No new sr image objects to process')
+        print('No export actions to process')
 
 
 app()
