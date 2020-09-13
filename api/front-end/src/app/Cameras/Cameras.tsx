3621b559cf271efae0405ac195bf1349eb2c13ce
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-13 16:18:50 +0300

    Working cameras view
    Does not have audio loading yet

--- api/front-end/src/app/Cameras/Cameras.tsx
+++ api/front-end/src/app/Cameras/Cameras.tsx
@@ -14,6 +14,7 @@ class Cameras extends Component {
   private _isMounted: boolean;
 
   state = {
+    windowWidth: window.innerWidth,
     imageData: [] as ImageDataInterface[]
   };
 
@@ -24,6 +25,7 @@ class Cameras extends Component {
 
   componentDidMount(): void {
     this._isMounted = true;
+    window.addEventListener("resize", this.handleResize);
     axios.post(GET_LATEST_CAMERA_IMAGES_PATH).then((data: any) => {
       if (this._isMounted) {
         this.setState({imageData: data.data.images});
@@ -35,16 +37,33 @@ class Cameras extends Component {
 
   componentWillUnmount(): void {
     this._isMounted = false;
+    window.removeEventListener("resize", this.handleResize);
   }
+
+  handleResize = () => {
+    this.setState({windowWidth: window.innerWidth});
+  };
 
   render() {
     const cameraFlexStyle = ["d-flex", "justify-content-center", "flex-wrap"];
     let cameraImages: JSX.Element[] = [<div key="null"/>]; // JSX.Element[] = [<span className={cameraFlexStyle.join(' ')}>No camera images available</span>];
 
+    const width = this.state.windowWidth / 2;
+    const height = width * 50 / 100;
+
     if (this.state.imageData !== undefined) {
       if (this.state.imageData.length > 0) {
         cameraImages = this.state.imageData.map(image => {
-          return <div key={image.id}>{image.file_name}</div>
+          return (
+            <img
+              id={image.id}
+              title={image.name}
+              className="CursorPointer"
+              style={{width: width, height: height}}
+              key={image.id}
+              src={image.image}
+              alt={image.file_name}/>
+          )
         });
       }
     }
