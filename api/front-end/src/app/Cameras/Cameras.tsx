83467fdea8dde85ecde0c0058501b6f4dcd93334
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-13 13:10:45 +0300

    Developing cameras view

--- api/front-end/src/app/Cameras/Cameras.tsx
+++ api/front-end/src/app/Cameras/Cameras.tsx
@@ -1,10 +1,44 @@
 import React, {Component} from "react";
 import styles from './Cameras.module.css'
+import axios, {GET_LATEST_CAMERA_IMAGES_PATH} from '../../axios';
+
+interface ImageDataInterface {
+  id: string,
+  name: string,
+  image: string, // this contains image data, long base64 string
+  file_name: string,
+  file_create_date: string
+}
 
 class Cameras extends Component {
+  state = {
+    imageData: [] as ImageDataInterface[]
+  };
+
+  componentDidMount(): void {
+    axios.post(GET_LATEST_CAMERA_IMAGES_PATH).then((data: any) => {
+      this.setState({imageData: data.data.images})
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
   render() {
+    const cameraFlexStyle = ["d-flex", "justify-content-center", "flex-wrap", "CameraFlexStyle"];
+    let cameraImages: JSX.Element[] = [<span>No camera images available</span>];
+
+    if (this.state.imageData !== undefined) {
+      cameraImages = this.state.imageData.map(image => {
+        return <div key={image.id}>{image.file_name}</div>
+      });
+    }
+
     return (
-      <div>Cameras page</div>
+      <div className={styles.Cameras}>
+        <div className={cameraFlexStyle.join(' ')}>
+          {cameraImages}
+        </div>
+      </div>
     )
   }
 }
--- api/front-end/src/axios.ts
+++ api/front-end/src/axios.ts
@@ -7,3 +7,9 @@ const instance = axios.create({
 // instance.defaults.headers.common['Someheader'] = 'somevalue';
 
 export default instance;
+
+// .env file in root can override default base path taken from window location href
+const basePath: string = process.env.REACT_APP_API_BASE_URL !== undefined ?
+  process.env.REACT_APP_API_BASE_URL : document.location.href;
+
+export const GET_LATEST_CAMERA_IMAGES_PATH = basePath + "get/latest/camera/images";
