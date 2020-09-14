a6546d303943ff64e2b450297efbfc6269d6536b
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-14 20:16:01 +0300

    Create BigImage.tsx

---
+++ api/front-end/src/tools/BigImage/BigImage.tsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+/**
+ * This class idea is to show big object detection raw images
+ * anywhere where it's called
+ */
+class BigImage extends Component<any, any> {
+  render() {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default BigImage;
