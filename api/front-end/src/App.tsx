43db726b797442ec27d690442441a7d79f0264ad
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-14 16:23:07 +0300

    Remove router dom, bring functionalities to App tsx instead

--- api/front-end/src/App.tsx
+++ api/front-end/src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./app/Navbar/Navbar";
-import RouterDom from "./RouterDom";
+import {Switch, Route, useLocation} from "react-router-dom"
+import styles from './app/Cameras/Cameras.module.css'
+
+import Cameras from "./app/Cameras/Cameras";
+import Home from "./app/Home/Home";
+import Faces from "./app/Faces/Faces";
+
 
 function App() {
+  const location = useLocation();
+  let classes: string[] = [];
+
+  if (location.pathname === '/cameras' || location.pathname === '/faces') {
+    classes = [styles.Cameras];
+  }
+
   return (
     <div className="App">
       <NavBar/>
-      <RouterDom/>
+      <div className={classes.join(' ')}>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/cameras' component={Cameras}/>
+          <Route exact path='/faces' component={Faces}/>
+        </Switch>
+      </div>
     </div>
   );
 }
