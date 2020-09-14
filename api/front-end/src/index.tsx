fc7b44fda4c83eb1cfed4d97b03d96eec0be15d6
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-14 19:48:50 +0300

    Bring Magic css, will be used everywhere so imported globally

--- api/front-end/src/index.tsx
+++ api/front-end/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from "react-router-dom";
+import './Magic.css'
 
 ReactDOM.render(
   <React.StrictMode>
