cb662dad9fa67a5b77478a43569a25245b396b85
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-15 17:21:39 +0300

    Rename modal, call it as generic image modal
    Implementing as functional component

---
+++ api/front-end/src/tools/GenericImageModal/GenericImageModal.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Button, Modal} from "react-bootstrap";
+
+/**
+ * This class idea is to show big object detection raw images from anywhere where it's called
+ */
+export const GenericImageModal = (props: ModalPropsInterface) => {
+  return (
+    <>
+      <Modal show={props.show}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p className="text-muted">{props.description}</p>
+          <img className="card-img-right" style={{width: '100%', height: '100%'}}
+               alt="genericModalImage" src={props.src}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => props.closeHandler}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+};
+
+export interface ModalPropsInterface {
+  show: boolean;
+  title: string;
+  description: string;
+  src: string; // This is base64 encoded image data
+  closeHandler: void;
+}
