303686f1a47b1b4bc157e68929792365a4e14a79
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-16 19:49:18 +0300

    Update Labels.tsx

--- api/front-end/src/pages/Home/Labels/Labels.tsx
+++ api/front-end/src/pages/Home/Labels/Labels.tsx
@@ -1,46 +1,42 @@
 import React, {Component} from "react";
-import {Card, Container, Navbar} from "react-bootstrap";
+import {Badge, Card} from "react-bootstrap";
 import {Doughnut} from 'react-chartjs-2';
 import {LoadingIndicator} from "../../../components/LoadingIndicator/LoadingIndicator";
 import {
   getIntelligence,
-} from '../../../utils/Utils'
+  IntelligenceInterface,
+} from '../../../utils/Utils';
 
+export interface LabelDonutDataInterface {
+  labels: [],
+  datasets: [{
+    data: [],
+    backgroundColor: [],
+    hoverBackgroundColor: []
+  }]
+}
 
 class Labels extends Component {
   state = {
-    isLoading: false,
+    isLoading: true,
+    labelSelection: 'No label selected',
+    instanceCount: 0,
+    storageUse: 'N/A GB',
+    labelDonutData: {} as LabelDonutDataInterface,
   };
 
   componentDidMount(): void {
-    this.loadIntelligence('2020-09-16');
-    // const intervalId = setInterval(() => this.loadFaceImages(this.state.selectedDay), 60 * 1000);
-    // this.setState({intervalId: intervalId});
+    this.loadIntelligence('2020-09-16').then(() => null);
   }
 
   async loadIntelligence(date: string) {
-    const result = await getIntelligence(date);
-  };
-
-  data = {
-    labels: [
-      'Red',
-      'Green',
-      'Yellow'
-    ],
-    datasets: [{
-      data: [300, 50, 100],
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ],
-      hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ]
-    }]
+    const result = await getIntelligence(date) as IntelligenceInterface;
+    console.log(result);
+    this.setState({
+      isLoading: false,
+      instanceCount: result.performance.instanceCount,
+      storageUse: result.performance.storageUse,
+    });
   };
 
   render() {
@@ -49,12 +45,22 @@ class Labels extends Component {
     return (
       <div className="mt-2 mr-2 ml-2">
         <Card bg="dark" text="light">
-          <Card.Header>Label viewer</Card.Header>
+          <Card.Header>
+            <div className="row">
+              <div className="col-sm">
+                <b>Label viewer</b>
+              </div>
+              <div className="col-sm text-right">
+                <Badge variant="light" className="mr-2">IC{this.state.instanceCount}</Badge>
+                <Badge variant="light">STR {this.state.storageUse}</Badge>
+              </div>
+            </div>
+          </Card.Header>
           <Card.Body>
-            <Doughnut data={this.data} height={300} options={{ maintainAspectRatio: false }}/>
-
-            { /* Handle showing loading indicator */
-              this.state.isLoading ? <LoadingIndicator/> : null
+            {
+              this.state.labelDonutData.datasets !== undefined ?
+                <Doughnut data={this.state.labelDonutData} height={300} options={{maintainAspectRatio: true}}/>
+                : this.state.isLoading ? <LoadingIndicator/> : null
             }
 
             <div className="d-flex justify-content-center flex-wrap">
@@ -63,9 +69,14 @@ class Labels extends Component {
 
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small className="text-muted">{this.state.labelSelection}</small>
           </Card.Footer>
         </Card>
+
+        { /* Handle showing loading indicator */
+          this.state.isLoading ? <LoadingIndicator/> : null
+        }
+
       </div>
     )
   }
