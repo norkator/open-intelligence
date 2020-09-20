85ec479a17205562f6d083e0b54d554a002391b3
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-20 11:13:27 +0300

    Add test build react front end as current html build serving

---
+++ api/html/precache-manifest.5148a215297aae87e7acec999c39bbd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e87ad8872d9d978a0d3d6075dbed00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d6e92f07bc7bdcfc7db",
+    "url": "./static/css/2.4de482e8.chunk.css"
+  },
+  {
+    "revision": "1681849ffe1d9689471f",
+    "url": "./static/css/main.f694c1a6.chunk.css"
+  },
+  {
+    "revision": "1d6e92f07bc7bdcfc7db",
+    "url": "./static/js/2.933d9a61.chunk.js"
+  },
+  {
+    "revision": "99d925238b9a954de93fb2893d8c697b",
+    "url": "./static/js/2.933d9a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1681849ffe1d9689471f",
+    "url": "./static/js/main.e4d9a131.chunk.js"
+  },
+  {
+    "revision": "ba3b5bd43745e7647455",
+    "url": "./static/js/runtime-main.7f1d4352.js"
+  }
+]);
