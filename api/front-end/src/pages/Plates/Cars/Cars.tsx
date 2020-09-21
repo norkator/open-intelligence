4182dfee4d88a64278154bc4fb33406df02b83ed
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-21 21:09:40 +0300

    Update Cars.tsx

--- api/front-end/src/pages/Plates/Cars/Cars.tsx
+++ api/front-end/src/pages/Plates/Cars/Cars.tsx
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
 import {ReduxPropsInterface} from "../../../store/dateReducer";
+import {Card} from "react-bootstrap";
+import DateRangeSelector from "../../Home/DateRangeSelector/DateRangeSelector";
 
 class Cars extends Component<ReduxPropsInterface> {
+
+  // Todo, this is reference for this view: https://github.com/norkator/open-intelligence/blob/master/api/html/plates.html
 
   render() {
     return (
       <div>
-        Date range selector component and Cars view
+        <div className="magictime vanishIn">
+          <Card bg="Light" text="dark">
+            <Card.Header>
+              <b>Unknown cars</b>
+            </Card.Header>
+            <Card.Body style={{padding: '0px'}}>
+              <DateRangeSelector {...this.props} />
+
+              <b>Create materialistic view here showing ALPR text and vehicle image</b>
+
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">This view shows you cars which require your attention</small>
+            </Card.Footer>
+          </Card>
+        </div>
       </div>
     )
   }
