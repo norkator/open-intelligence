f0684be9113c4f93182ca353489616119b1bbcdb
Norkator <55850510+norkator@users.noreply.github.com>
2020-09-22 19:43:03 +0300

    Update intelligence.js

--- api/intelligence.js
+++ api/intelligence.js
@@ -33,7 +33,6 @@ initDb.initDatabase().then(() => {
     app.use(function(req, res, next) {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT');
-      
       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
       next();
     });
