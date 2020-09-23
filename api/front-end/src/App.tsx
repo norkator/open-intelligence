5f48272c04d5b28a733409e4295e4dfcc294393f
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2020-09-23 10:59:47 +0300

    Add training, add old plate training
    Plate training should not be programmed again with react

--- api/front-end/src/App.tsx
+++ api/front-end/src/App.tsx
@@ -4,15 +4,16 @@ import NavBar from "./pages/Navbar/Navbar";
 import {Switch, Route} from "react-router-dom"
 import styles from './pages/Cameras/Cameras.module.css'
 
+// Pages
 import Cameras from "./pages/Cameras/Cameras";
 import Home from "./pages/Home/Home";
 import Faces from "./pages/Faces/Faces";
 import Plates from "./pages/Plates/Plates";
+import Training from "./pages/Training/Training";
 
 
 function App() {
   let classes: string[] = [styles.Cameras];
-
   return (
     <div className="App">
       <NavBar/>
@@ -22,6 +23,7 @@ function App() {
           <Route exact path='/cameras' component={Cameras}/>
           <Route exact path='/plates' component={Plates}/>
           <Route exact path='/faces' component={Faces}/>
+          <Route exact path='/training' component={Training}/>
         </Switch>
       </div>
     </div>
---
+++ api/front-end/src/pages/Training/Training.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Training extends Component<any, any> {
+
+  componentDidMount(): void {
+  }
+
+  render() {
+    return (
+      <>
+
+      </>
+    )
+  }
+}
+
+
+export default Training;
