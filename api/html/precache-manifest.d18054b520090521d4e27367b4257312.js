1689a3c031e80efb163b509b6ba4c6015ec0c9f2
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2020-09-28 19:40:16 +0300

    Next version build ready for use

--- api/html/precache-manifest.d18054b520090521d4e27367b4257312.js
+++ api/html/precache-manifest.d18054b520090521d4e27367b4257312.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82bd53b05103efee2748c6a81d33727f",
+    "revision": "1015475b8b7303f51b0226bde60b03c9",
     "url": "./index.html"
   },
   {
-    "revision": "dc826798860be1bed7e9",
+    "revision": "dae0459f2f423941f4ac",
     "url": "./static/css/2.4de482e8.chunk.css"
   },
   {
-    "revision": "b9842355be1e4dee6932",
+    "revision": "21681e98abfae28929c3",
     "url": "./static/css/main.2048abae.chunk.css"
   },
   {
-    "revision": "dc826798860be1bed7e9",
-    "url": "./static/js/2.04d63fcf.chunk.js"
+    "revision": "dae0459f2f423941f4ac",
+    "url": "./static/js/2.1473d5c0.chunk.js"
   },
   {
     "revision": "99d925238b9a954de93fb2893d8c697b",
-    "url": "./static/js/2.04d63fcf.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.1473d5c0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b9842355be1e4dee6932",
-    "url": "./static/js/main.47734188.chunk.js"
+    "revision": "21681e98abfae28929c3",
+    "url": "./static/js/main.26298e41.chunk.js"
   },
   {
     "revision": "ba3b5bd43745e7647455",
