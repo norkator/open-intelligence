cd8cf0336a8c8f016417e38f2ac15c8020facdb7
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-10-28 22:10:07 +0300

    add cleaning page skeleton

---
+++ api/front-end/src/pages/Cleaning/Cleaning.tsx
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import {WithTranslation, withTranslation} from "react-i18next";
+import {Button, Card} from "react-bootstrap";
+import {LoadingIndicator} from "../../components/LoadingIndicator/LoadingIndicator";
+import toast, {Toaster} from "react-hot-toast";
+
+
+class Cleaning extends Component<WithTranslation> {
+  private _isMounted: boolean;
+
+  state = {
+    isLoading: true,
+  };
+
+  constructor(props: any) {
+    super(props);
+    this._isMounted = false;
+  }
+
+  componentDidMount(): void {
+    this._isMounted = true;
+  }
+
+  componentWillUnmount(): void {
+    this._isMounted = false;
+  }
+
+  render() {
+    const {t} = this.props;
+    return (
+      <div className="mt-2 mr-2 ml-2">
+        <Card bg="dark" text="light">
+          <Card.Header>
+            Title
+          </Card.Header>
+          <Card.Body style={{padding: '10px'}}>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  }
+}
+
+// @ts-ignore
+export default withTranslation('i18n')(Cleaning);
