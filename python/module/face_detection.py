8e6fb87b897b4727c73858325e71a01b5ae36e0e
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2022-10-05 16:57:25 +0300

    remove some old code

--- python/module/face_detection.py
+++ python/module/face_detection.py
@@ -9,7 +9,6 @@ output_root_folder_path = database.find_config_value(app_config, 'output_folder'
 # Paths
 person_path = output_root_folder_path + 'person/'
 output_faces_path = output_root_folder_path + 'faces/'
-classifier_path = os.getcwd() + '/classifiers/'
 
 
 # Main method to call
@@ -23,47 +22,4 @@ def recognize_person(image_file_path_name_extension, output_file_name):
         )
     except Exception as e:
         print(e)
-    '''
-    if detection_name_and_probability is None:
-        haarcascade_face_detection(image_file_path_name_extension, output_file_name)
-        return ''
-    else:
-    '''
     return detection_name_and_probability
-
-
-# If person recognition fails (face + person)
-# then try detect only face with haarcascade, write small image out
-# TODO: validate later is this section usable anywhere
-'''
-def haarcascade_face_detection(image_file_path_name_extension, output_file_name):
-    object_cascade = cv2.CascadeClassifier(classifier_path + 'haarcascade_frontalface_default.xml')
-
-    img = cv2.imread(image_file_path_name_extension)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face_img = img.copy()
-
-    # image, reject levels level weights.
-    faces = object_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    # add this
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 1)
-        roi = face_img[y:y + h, x:x + w]
-
-        # Write small face images
-        try:
-            Path(output_faces_path).mkdir(parents=True, exist_ok=True)
-            cv2.imwrite(output_faces_path + output_file_name, roi)
-        except Exception as e:
-            print(e)
-
-    cv2.imshow('face', img)
-    cv2.waitKey(1)  # no freeze, refreshes for a millisecond
-'''
