337bd2b7291d80bddd58df7b4e505a563ed77afc
Martin Kankaanranta <martin.kankaanranta@hotmail.com>
2022-10-05 17:24:57 +0300

    re organize recognize logic

--- python/module/face_recognition/recognize.py
+++ python/module/face_recognition/recognize.py
@@ -52,93 +52,99 @@ def recognize(output_file_name=None, input_confidence=0.5, input_image=None):
     embedding_model_path = os.getcwd() + '/models/face_detection_model/' + 'openface_nn4.small2.v1.t7'
     embedder = cv2.dnn.readNetFromTorch(embedding_model_path)
 
-    # load the actual face recognition model along with the label encoder
-    recognizer = pickle.loads(open(recognizer_path, "rb").read())
-    label_encoder = pickle.loads(open(label_encoder_path, "rb").read())
-
     # load the image, resize it to have a width of 600 pixels (while
     # maintaining the aspect ratio), and then grab the image dimensions
     image = cv2.imread(input_image)
     image = imutils.resize(image, width=600)
     (h, w) = image.shape[:2]
 
-    # construct a blob from the image
-    image_blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False
-    )
+    # This might be first run time without trained model yet, so catch exception
+    try:
+        # load the actual face recognition model along with the label encoder
+        recognizer = pickle.loads(open(recognizer_path, "rb").read())
+        label_encoder = pickle.loads(open(label_encoder_path, "rb").read())
 
-    # apply OpenCV's deep learning-based face detector to localize
-    # faces in the input image
-    detector.setInput(image_blob)
-    detections = detector.forward()
+        # construct a blob from the image
+        image_blob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False
+        )
 
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the
-        # prediction
-        confidence = detections[0, 0, i, 2]
+        # apply OpenCV's deep learning-based face detector to localize
+        # faces in the input image
+        detector.setInput(image_blob)
+        detections = detector.forward()
 
-        # filter out weak detections
-        if confidence > input_confidence:
-            # compute the (x, y)-coordinates of the bounding box for the
-            # face
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        # loop over the detections
+        for i in range(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with the
+            # prediction
+            confidence = detections[0, 0, i, 2]
 
-            # extract the face ROI
-            face = image[startY:endY, startX:endX]
-            (fH, fW) = face.shape[:2]
+            # filter out weak detections
+            if confidence > input_confidence:
+                # compute the (x, y)-coordinates of the bounding box for the
+                # face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-            # ensure the face width and height are sufficiently large
-            if fW < 20 or fH < 20:
-                continue
+                # extract the face ROI
+                face = image[startY:endY, startX:endX]
+                (fH, fW) = face.shape[:2]
 
-            # construct a blob for the face ROI, then pass the blob
-            # through our face embedding model to obtain the 128-d
-            # quantification of the face
-            face_blob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96), (0, 0, 0), swapRB=True, crop=False)
-            embedder.setInput(face_blob)
-            vec = embedder.forward()
+                # ensure the face width and height are sufficiently large
+                if fW < 20 or fH < 20:
+                    continue
 
-            # perform classification to recognize the face
-            preds = recognizer.predict_proba(vec)[0]
-            j = np.argmax(preds)
-            proba = preds[j]
-            name = label_encoder.classes_[j]
+                # construct a blob for the face ROI, then pass the blob
+                # through our face embedding model to obtain the 128-d
+                # quantification of the face
+                face_blob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96), (0, 0, 0), swapRB=True, crop=False)
+                embedder.setInput(face_blob)
+                vec = embedder.forward()
 
-            # draw the bounding box of the face along with the associated
-            # probability
-            text = "{}: {:.2f}%".format(name, proba * 100)
-            detection_name_and_probability = text
-            print("[INFO] " + text)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 255), 2)
-            cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 255), 2)
+                # perform classification to recognize the face
+                preds = recognizer.predict_proba(vec)[0]
+                j = np.argmax(preds)
+                proba = preds[j]
+                name = label_encoder.classes_[j]
 
-            # Write small face images
-            if output_file_name is not None:
-                try:
-                    Path(output_faces_path).mkdir(parents=True, exist_ok=True)
-                    cv2.imwrite(output_faces_path + file_name_prefix + output_file_name, face)
-                except Exception as e:
-                    print(e)
+                # draw the bounding box of the face along with the associated
+                # probability
+                text = "{}: {:.2f}%".format(name, proba * 100)
+                detection_name_and_probability = text
+                print("[INFO] " + text)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 255), 2)
+                cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 255), 2)
 
-            # Copy original image to faces dataset for later training,
-            # also create image having bounding box for front end ui validation
-            try:
-                Path(output_faces_dataset).mkdir(parents=True, exist_ok=True)
-                shutil.copy(input_image, output_faces_dataset + file_name_prefix + output_file_name)
-                cv2.imwrite(output_faces_dataset + 'RECT_' + file_name_prefix + output_file_name, image)
-            except Exception as e:
-                print(e)
+                # Write small face images
+                if output_file_name is not None:
+                    try:
+                        Path(output_faces_path).mkdir(parents=True, exist_ok=True)
+                        cv2.imwrite(output_faces_path + file_name_prefix + output_file_name, face)
+                    except Exception as e:
+                        print(e)
 
-    # show the output image
-    if cv2_imshow_enabled is True:
-        cv2.imshow("PickleFaceRec", image)
-        cv2.waitKey(1)  # no freeze, refreshes for a millisecond
+        # show the output image
+        if cv2_imshow_enabled is True:
+            cv2.imshow("PickleFaceRec", image)
+            cv2.waitKey(1)  # no freeze, refreshes for a millisecond
 
-    # Return result
-    return detection_name_and_probability
+        # Return result
+        return detection_name_and_probability
+    except Exception as e:
+        print(e)
+        return None
+    finally:
+        # Copy original image to faces dataset for later training,
+        # also create image having bounding box for front end ui validation
+        # even in a exception event this is needed for first time training reasons
+        try:
+            Path(output_faces_dataset).mkdir(parents=True, exist_ok=True)
+            shutil.copy(input_image, output_faces_dataset + file_name_prefix + output_file_name)
+            cv2.imwrite(output_faces_dataset + 'RECT_' + file_name_prefix + output_file_name, image)
+        except Exception as e:
+            print(e)
 
 
 # Customized to suit for insight face
